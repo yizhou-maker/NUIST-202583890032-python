20356dc67b043b358a969c0299122e37f632ec68
zhouyi <3328223866@qq.com>
2026-03-19 19:23:13 +0800

    version2 and 3

--- AddTwoNumbers.py
+++ AddTwoNumbers.py
@@ -1,3 +1,12 @@
 num1=5
 num2=3
 print("sum:",num1+num2)
+num1=float(input("enter first numbers:"))
+num2=float(input("enter second numbers:"))
+print("sum:",num1+num2)
+try:
+    num1=float(input("enter first numbers:"))
+    num2=float(input("enter second numbers:"))
+    print("sum:",num1+num2)
+except ValueError:
+    print("Please enter valid numbers.")
